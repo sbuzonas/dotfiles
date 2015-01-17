3ed3469ff6710fb4b7e99f9d77331429ccd10b97
Steve Buzonas <steve@fancyguy.com>
2015-01-16 19:12:51 -0500

    update post create project command to run application

---
+++ src/Dotfiles/Composer/EventHandler.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * This file is part of Dotfiles.
+ *
+ * (c) Steve Buzonas <steve@fancyguy.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Dotfiles\Composer;
+
+use Composer\IO\ConsoleIO;
+use Composer\Script\Event;
+use Dotfiles\Console\Application;
+use Symfony\Component\Console\Input\StringInput;
+use Symfony\Component\Console\Output\ConsoleOutput;
+
+class EventHandler
+{
+
+	public static function postCreateProject(Event $event)
+	{
+		$io = $event->getIO();
+		if ($io instanceof ConsoleIO) {
+			$application = new Application();
+			$verbosity = '';
+			switch (true) {
+			case $io->isDebug():
+				$verbosity .= 'v';
+				// no break
+			case $io->isVeryVerbose():
+				$verbosity .= 'v';
+				// no break
+			case $io->isVerbose():
+				$verbosity = ' -v' . $verbosity;
+			}
+			$input = new StringInput('install'.$verbosity);
+			$output = new ConsoleOutput();
+			$application->run($input, $output);
+		}
+	}
+	
+}
