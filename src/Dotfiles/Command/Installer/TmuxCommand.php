0b2fac9331002d2a1ad4dfc5144fd41b061bd345
Steve Buzonas <steve@fancyguy.com>
2015-01-17 22:20:00 -0500

    update license text

--- src/Dotfiles/Command/Installer/TmuxCommand.php
+++ src/Dotfiles/Command/Installer/TmuxCommand.php
@@ -1,9 +1,8 @@
 <?php
-
 /**
  * This file is part of Dotfiles.
  *
- * (c) Steve Buzonas <steve@fancyguy.com
+ * (c) Steve Buzonas <steve@fancyguy.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
