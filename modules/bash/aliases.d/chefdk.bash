65c34fe7b4074d7bb28bc9404997fdaa71b7ff99
Steve Buzonas <steve@fancyguy.com>
2015-05-01 19:03:48 -0400

    add carnegie learning aliases to chefdk

--- modules/bash/aliases.d/chefdk.bash
+++ modules/bash/aliases.d/chefdk.bash
@@ -16,6 +16,16 @@ alias cg-lwrp='chef generate lwrp -I mit -C "Steve Buzonas" -m steve@fancyguy.co
 alias cg-repo='chef generate repo -I mit -C "Steve Buzonas" -m steve@fancyguy.com'
 alias cg-policyfile='chef generate policyfile -I mit -C "Steve Buzonas" -m steve@fancyguy.com'
 
+alias cli-app='chef generate app -I all_rights -C "Carnegie Learning, Inc." -m sbuzonas@carnegielearning.com -g ~/.chef/cli_chef_generator/'
+alias cli-cookbook='chef generate cookbook -I all_rights -C "Carnegie Learning, Inc." -m sbuzonas@carnegielearning.com -g ~/.chef/cli_chef_generator/'
+alias cli-recipe='chef generate recipe -I all_rights -C "Carnegie Learning, Inc." -m sbuzonas@carnegielearning.com -g ~/.chef/cli_chef_generator/'
+alias cli-attribute='chef generate attribute -I all_rights -C "Carnegie Learning, Inc." -m sbuzonas@carnegielearning.com -g ~/.chef/cli_chef_generator/'
+alias cli-template='chef generate template -I all_rights -C "Carnegie Learning, Inc." -m sbuzonas@carnegielearning.com -g ~/.chef/cli_chef_generator/'
+alias cli-file='chef generate file -I all_rights -C "Carnegie Learning, Inc." -m sbuzonas@carnegielearning.com -g ~/.chef/cli_chef_generator/'
+alias cli-lwrp='chef generate lwrp -I all_rights -C "Carnegie Learning, Inc." -m sbuzonas@carnegielearning.com -g ~/.chef/cli_chef_generator/'
+alias cli-repo='chef generate repo -I all_rights -C "Carnegie Learning, Inc." -m sbuzonas@carnegielearning.com -g ~/.chef/cli_chef_generator/'
+alias cli-policyfile='chef generate policyfile -I all_rights -C "Carnegie Learning, Inc." -m sbuzonas@carnegielearning.com -g ~/.chef/cli_chef_generator/'
+
 # Local Variables:
 # coding: utf-8
 # mode: sh
--- modules/bash/path.d/default.bash
+++ modules/bash/path.d/default.bash
@@ -2,6 +2,7 @@ PATH="$(path_prepend /usr/local/bin)"
 PATH="$(path_prepend $DOTFILES_DIR/bin)"
 PATH="$(path_prepend $HOME/bin)"
 PATH="$(path_prepend $HOME/.rbenv/bin)"
+PATH="$(path_prepend $HOME/.composer/vendor/bin)"
 PATH="$(path_prepend ./bin)"
 
 export PATH="$(deduplicate_path $PATH)"
